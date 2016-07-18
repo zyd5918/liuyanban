ccc232bf4d7192ebc524dae143bd05b4e2f6a3ac
arivin <btc022003@gmail.com>
2016-07-18 09:17:05 +0800

    当redirect做页面跳转的时候,如果参数中有中文 需要进行编码

--- app.js
+++ app.js
@@ -21,8 +21,8 @@ app.engine('.html', template.__express);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'html');
 
-function initNoteApp(req,res,next){
-  /////判断目录是否存在
+function initNoteApp(req, res, next) {
+    /////判断目录是否存在
     fs.exists('./notes', function (d) {
         if (d) {
             console.log('目录已存在');
@@ -50,12 +50,19 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/',initNoteApp,(req,res)=>{
-  res.redirect('/dir.html');
+/****
+ * 在express中使用res.redirect 方式做页面跳转的时候 
+ * 如果在url中传递的有中文参数 需要把参数进行一个编码操作
+ * 使用 encodeURIComponent方法实现url参数编码
+ */
+app.get('/', initNoteApp, (req, res) => {
+    // var p = encodeURIComponent('有一个新的文件夹')
+    // res.redirect('/notes/file/'+p);
+    res.redirect('/dir.html');
 });
 
-app.use('/notes/dir',require('./routes/dir'));
-app.use('/notes/file',require('./routes/file'))
+app.use('/notes/dir', require('./routes/dir'));
+app.use('/notes/file', require('./routes/file'))
 
 //app.use('/', routes);
 //app.use('/users', users);
@@ -95,6 +102,6 @@ app.use('/notes/file',require('./routes/file'))
 // module.exports = app;
 
 /////在3000端口启动服务器
-app.listen(3000,function(request,response){
-  console.log('服务器运行中......');
+app.listen(3000, function (request, response) {
+    console.log('服务器运行中......');
 })
